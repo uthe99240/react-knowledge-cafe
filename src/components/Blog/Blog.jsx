da35bedfe938843e770d2f81c4537ae381861b6a
DESKTOP-C5A1H8M\Admin <faisalahmed99240@gmail.com>
2024-11-03 13:33:22 +0600

    ---

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,15 +1,19 @@
 import PropTypes from 'prop-types';
 
-const Blog = ({blog}) => {
+const Blog = ({ blog }) => {
+
+    const { title } = blog;
     return (
         <div>
-            
+            <h2 className='text-4xl'>{title}</h2>
         </div>
     );
 };
 
-Blog.PropTypes = {
-    blog:PropTypes.object.isRequired
-}
+Blog.propTypes = {
+    blog: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+    }).isRequired,
+};
 
 export default Blog;
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
 
@@ -13,6 +14,9 @@ const Blogs = () => {
     return (
         <div className="md:w-2/3">
             <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+            {
+                blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
+            }
         </div>
     );
 };
