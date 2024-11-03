4b7003580069aa6e6a86f5a48e56d800ced63133
DESKTOP-C5A1H8M\Admin <faisalahmed99240@gmail.com>
2024-11-03 12:41:28 +0600

    first commit

---
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -0,0 +1,10 @@
+
+const Bookmarks = () => {
+    return (
+        <div className='w-1/3'>
+            <h1 className="text-4xl">Bookmarks</h1>
+        </div>
+    );
+};
+
+export default Bookmarks;
